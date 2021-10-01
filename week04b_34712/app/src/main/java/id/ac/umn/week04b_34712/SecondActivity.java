615cd65a389589633ff56ba07e7ad3ec8a17acf5
adiptamuhammad <84858701+adiptamuhammad@users.noreply.github.com>
2021-10-01 22:19:22 +0700

    Week04

---
+++ week04b_34712/app/src/main/java/id/ac/umn/week04b_34712/SecondActivity.java
@@ -0,0 +1,13 @@
+package id.ac.umn.week04b_34712;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
